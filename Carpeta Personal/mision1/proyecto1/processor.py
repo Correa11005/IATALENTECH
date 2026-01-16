a3754e995acb15f86d547d4c78d9d8f0e2bf7de1
Jacob Correa <jacobocorrea777@gmail.com>
2026-01-16 11:04:49 -0500

    cambios nuevos

--- Carpeta Personal/mision1/proyecto1/processor.py
+++ Carpeta Personal/mision1/proyecto1/processor.py
@@ -1,7 +1,20 @@
 import re
+# ==============================
+# FUNCION
+# Elimina caracteres no numericos de un documento
+#cc75.888.56 = "7588856"
+# ==============================
 def clean_id(value):
-    # Elimina caracteres no numericos de un documento
     if value is None: 
         return ""
     return re.sub(r"\D",'',str(value))
-    
+# ==============================
+#  FUNCION merge _name 
+#  Une nombre y apellido en un solo campo 
+# ==============================
+def merge_name(name, lastname): 
+    if name is None:
+        name= ""
+    if lastname is None:
+        lastname= ""
+    return f"{name} {lastname}".strip()
--- Carpeta Personal/mision1/proyecto1/test_processor.py
+++ Carpeta Personal/mision1/proyecto1/test_processor.py
@@ -1,5 +1,7 @@
-from processor import clean_id 
+from processor import clean_id , merge_name
 def test_clean_id():
     assert clean_id("cc-75.087.345")=="75087345"
-    
+def test_merge_name():
+    assert merge_name("Jacobo","Correa")=="Jacobo Correa"
+
 
